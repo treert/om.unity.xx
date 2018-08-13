317e4c7a99164ae0df25cd275067577d35d308b8
onemore <treertzhu@123u.com>
2018-08-13 12:34:56 +0800

    [add] Test Scene 用于写些临时测试代码

---
+++ LearnUnity2017/Assets/Scripts/GUIToolScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIToolScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    GameObject _last_test_mono_obj = null;
+    private void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("AddTestMono"/*, GUILayout.Width(80)*/))
+        {
+            var go = new GameObject("TestMono");
+            go.AddComponent<TestMonoScript>();
+            Debug.Log("After AddTestMono");
+            _last_test_mono_obj = go;
+        }
+        if (GUILayout.Button("InstanceMono"/*, GUILayout.Width(80)*/))
+        {
+            if(_last_test_mono_obj != null)
+            {
+                var go = GameObject.Instantiate<GameObject>(_last_test_mono_obj);
+                Debug.Log("After InstanceMono");
+                _last_test_mono_obj = go;
+            }
+        }
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ LearnUnity2017/Assets/Scripts/TestMonoScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMonoScript : MonoBehaviour {
+
+    private void Awake()
+    {
+        Debug.Log("TestMono Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("TestMono OnEnable");
+    }
+    // Use this for initialization
+    void Start () {
+        Debug.Log("TestMono Start");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
