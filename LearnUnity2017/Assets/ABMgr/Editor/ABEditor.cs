0233d6304b261b7e117818f496b3eae86cd28bf3
onemore <treertzhu@123u.com>
2018-08-31 17:14:18 +0800

    [add] AssetBundle相关

---
+++ LearnUnity2017/Assets/ABMgr/Editor/ABEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ABBuild : EditorWindow {
+
+    [MenuItem("ABMgr/OpenABBuild", priority = 1)]
+    static void OpenWin()
+    {
+        // Get existing open window or if none, make a new one:
+        var window = EditorWindow.GetWindow<ABBuild>("ABBuild");
+        window.minSize = new Vector2(300, 600);
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Build",GUILayout.Width(100)))
+        {
+            EditorUtility.DisplayDialog("提示确认", "确认", "OK");
+        }
+        GUILayout.EndHorizontal();
+    }
+}
