d8e7cb5e0d2ba48804d3fef1537ec35927cfea9a
onemore <treertzhu@123u.com>
2018-09-07 11:37:31 +0800

    【ABMgr】构建assetbundle

--- LearnUnity2017/Assets/ABMgr/Editor/ABEditor.cs
+++ LearnUnity2017/Assets/ABMgr/Editor/ABEditor.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -19,8 +21,48 @@ public class ABBuild : EditorWindow {
         GUILayout.BeginHorizontal();
         if (GUILayout.Button("Build",GUILayout.Width(100)))
         {
+            TestBuild();
             EditorUtility.DisplayDialog("提示确认", "确认", "OK");
         }
         GUILayout.EndHorizontal();
+    }
+
+    void TestBuild()
+    {
+        List<AssetBundleBuild> list = new List<AssetBundleBuild>();
+        EnumObjectsFromDir("Assets/ABMgr/Res", (path) => {
+            AddBuild(list, path, Path.GetFileName(path));
+        });
+        Directory.CreateDirectory("AssetBundles");
+        var meta = BuildPipeline.BuildAssetBundles("AssetBundles",list.ToArray(), BuildAssetBundleOptions.UncompressedAssetBundle, EditorUserBuildSettings.activeBuildTarget);
+        string ab_path = "AssetBundles/imgcontain12.prefab";
+        Hash128 hash1 = new Hash128();
+        if(BuildPipeline.GetHashForAssetBundle(ab_path, out hash1))
+        {
+        }
+
+        Hash128 hash2 = meta.GetAssetBundleHash("imgcontain12.prefab");
+
+        var abs = meta.GetAllAssetBundles();
+    }
+
+    void EnumObjectsFromDir(string dir, Action<string> cb)
+    {
+        var files = Directory.GetFiles(dir, "*", SearchOption.AllDirectories);
+        foreach(var f in files)
+        {
+            if (f.EndsWith(".meta") == false)
+            {
+                cb(f.Replace('\\','/'));
+            }
+        }
+    }
+
+    void  AddBuild(List<AssetBundleBuild> list, string path, string bundle_name)
+    {
+        AssetBundleBuild ret = new AssetBundleBuild();
+        ret.assetBundleName = bundle_name;
+        ret.assetNames = new string[] { path };
+        list.Add(ret);
     }
 }
