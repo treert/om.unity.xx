b4f14b53a80e8c5fef7a6031f583236c824ca127
onemore <treertzhu@123u.com>
2018-12-19 11:03:30 +0800

    【DemoEditor】辅助线

--- LearnUnity2017/Assets/DemoEditor/Editor/DemoInspectorEditor.cs
+++ LearnUnity2017/Assets/DemoEditor/Editor/DemoInspectorEditor.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class DemoInspectorEditor : MonoBehaviour {
+/// <summary>
+/// 参考资料：
+/// 1. Unity中常用的编辑器图形辅助线类 https://blog.csdn.net/Teddy_k/article/details/82117420
+/// 
+/// </summary>
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+[CustomEditor(typeof(DemoMonoScript))]
+public class DemoInspectorEditor : Editor {
+
+    DemoMonoScript myHandles;
+    GUIStyle style;
+    void OnEnable()
+    {
+        style = new GUIStyle();
+        style.richText = true;
+        style.fontSize = 20;
+        style.alignment = TextAnchor.MiddleCenter;
+        myHandles = (DemoMonoScript)target;
+    }
+
+    private void OnSceneGUI() // 选中时在Scene视图创建一个可调节Radius的Handle，调节后Player的radius值会改变
+    {
+        Handles.color = Color.white;
+        myHandles.radius = UnityEditor.Handles.RadiusHandle(Quaternion.identity, myHandles.transform.position, myHandles.radius);
+    }
 }
