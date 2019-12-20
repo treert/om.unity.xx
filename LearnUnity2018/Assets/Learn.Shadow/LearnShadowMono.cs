50b29a511a7aeafe0c5afa249551d63737685857
onemore <treertzhu@123u.com>
2019-12-20 17:00:12 +0800

    + 方差阴影，VSM

---
+++ LearnUnity2018/Assets/Learn.Shadow/LearnShadowMono.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnShadowMono : MonoBehaviour
+{
+    public Camera m_camera;
+    //public Transform m_light;
+    public RenderTexture m_rt;
+    public Shader m_shader;
+
+    public Material m_GaussMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shader.SetGlobalTexture("_CustomShadowMapTexture", m_rt);
+
+        m_camera.SetReplacementShader(m_shader, "RenderType");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //m_camera.transform.position = m_light.transform.position;
+        //m_camera.transform.rotation = m_light.transform.rotation;
+        //m_camera.transform.localScale = m_light.transform.localScale;
+        Shader.SetGlobalMatrix("_Custom_World2Shadow", m_camera.projectionMatrix * m_camera.worldToCameraMatrix);
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Replace"))
+        {
+            m_camera.SetReplacementShader(m_shader, "RenderType");
+        }
+        if (GUILayout.Button("Reset"))
+        {
+            m_camera.ResetReplacementShader();
+
+        }
+        GUILayout.EndHorizontal();
+    }
+
+    // 对方差阴影贴图做 高斯模糊
+}
