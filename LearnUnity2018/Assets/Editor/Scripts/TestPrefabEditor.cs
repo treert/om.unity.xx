6c6278b1a238ba71d906f826d93e55e6539c06dd
onemore <treertzhu@123u.com>
2021-03-04 19:02:14 +0800

    零散的修改，准备迁移到2019了。

---
+++ LearnUnity2018/Assets/Editor/Scripts/TestPrefabEditor.cs
@@ -0,0 +1,51 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
+using System;
+
+[InitializeOnLoad]
+public class PrefabStageListener : Editor
+{
+    static PrefabStageListener()
+    {
+        // 打开Prefab编辑界面回调
+        PrefabStage.prefabStageOpened += OnPrefabStageOpened;
+        // Prefab被保存之前回调
+        PrefabStage.prefabSaving += OnPrefabSaving;
+        // Prefab被保存之后回调
+        PrefabStage.prefabSaved += OnPrefabSaved;
+        // 关闭Prefab编辑界面回调
+        PrefabStage.prefabStageClosing += OnPrefabStageClosing;
+
+        PrefabUtility.prefabInstanceUpdated += OnPrefabInstanceUpdated;
+    }
+
+    private static void OnPrefabInstanceUpdated(GameObject instance)
+    {
+        Debug.Log(nameof(OnPrefabInstanceUpdated));
+    }
+
+    private static void OnPrefabStageClosing(PrefabStage obj)
+    {
+        Debug.Log(nameof(OnPrefabStageClosing));
+    }
+
+    private static void OnPrefabSaved(GameObject obj)
+    {
+        Debug.Log(nameof(OnPrefabSaved));
+    }
+
+    private static void OnPrefabSaving(GameObject obj)
+    {
+        Debug.Log(nameof(OnPrefabSaving));
+    }
+
+    private static void OnPrefabStageOpened(PrefabStage obj)
+    {
+        Debug.Log(nameof(OnPrefabStageOpened));
+    }
+}
+#endif
---
+++ LearnUnity2018/Assets/Scripts/TestMono.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestMono : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Image img;
+        Debug.Log($"TestMono Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"TestMono Update");
+    }
+}
